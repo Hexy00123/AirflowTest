9d3892cf1af493cecce4996c6459f466e72ca9a0
Ruslan <r.izmajlov2018@yandex.ru>
2024-07-10 19:41:57 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM apache/airflow:latest
+
+
+USER root
+RUN apt-get update && \
+    apt-get  -y install git && \
+    apt-get clean
+
+USER airflow
+
+WORKDIR project
+
+COPY ./requirements.txt ./
+RUN pip install -r requirements.txt
+
+COPY services/airflow/dags/ /usr/local/airflow/dags/
+COPY src/ /usr/local/airflow/src/
---
+++ services/airflow/dags/python_dag.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+import sys
+import os
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from src.data import get_data
+
+
+
+dafault_args = {
+    'owner': 'hexy',
+    'retries': 1,
+    'retry_delay': timedelta(seconds=10),
+}
+
+
+def greet(name):
+    print(f'Hello, {name}!')
+
+
+def get_data_():
+    print(os.getcwd())
+
+
+with DAG(
+    dag_id='TestPythonDAG',
+    description='Just temporary python dag for testing',
+    start_date=datetime(2024, 7, 9),
+    schedule_interval=None,
+    default_args=dafault_args
+) as dag:
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet,
+        op_kwargs={
+            'name': 'Ruslan'
+        }
+    )
+
+    task2 = PythonOperator(
+        task_id='get_data',
+        python_callable=get_data_,
+    )
+
+    task3 = BashOperator(
+        task_id='ls',
+        bash_command='ls -a',
+    )
+
+    task4 = BashOperator(
+        task_id='pwd',
+        bash_command='pwd',
+    )
+
+    task1 >> task2 >> task3 >> task4
+
+if __name__ == "__main__":
+    print(get_data())
